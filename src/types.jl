f4531af0d36b964bf2f82b06b6a4970d2b061295
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2026-07-03 10:12:05 +0200

    Simplify get_registry_default_branch.

--- src/types.jl
+++ src/types.jl
@@ -22,12 +22,7 @@ function path(cache::RegistryCache, registry_url::AbstractString)
 end
 
 function get_registry_default_branch(git::Cmd)
-    env = copy(ENV)
-    env["LANG"] = "en_US.UTF-8"
-    lines = collect(eachline(setenv(`$git remote show origin`, env)))
-    idx = findfirst(x -> occursin("HEAD branch", x), lines)
-    idx === nothing && error("Failed to get default branch of registry")
-    strip(split(lines[idx], ":")[2])
+    readchomp(`$git rev-parse --abbrev-ref HEAD`)
 end
 
 struct Project
